2dc9b79367168c6c1812c5369bf3ed57b5272ef2
jonatasneres <jonatas102015@hotmail.com>
2024-03-10 20:13:52 -0300

    controle de proteção de rotas adicionado login x register

--- front/app/login/page.tsx
+++ front/app/login/page.tsx
@@ -1,8 +1,19 @@
-// import LoginForm from "@/components/loginForm";
 import { NextPage } from "next";
 import LoginForm from "../components/loginForm";
+import { getCookie } from "cookies-next";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+const verifyToken = () => {
+  const token = getCookie("contatos.token", { cookies });
+  if (token) {
+    redirect("/principal");
+  }
+  return token;
+};
 
 const Login: NextPage = () => {
+  verifyToken();
   return (
     <>
       <main className="body min-h-screen flex items-center justify-center">
--- front/app/register/page.tsx
+++ front/app/register/page.tsx
@@ -1,13 +1,24 @@
-import RegisterForm from "../components/registerForm";;
+import { getCookie } from "cookies-next";
+import RegisterForm from "../components/registerForm";
 import { NextPage } from "next";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
+const verifyToken = () => {
+  const token = getCookie("contatos.token", { cookies });
+  if (token) {
+    redirect("/principal");
+  }
+  return token;
+};
 
 const Register: NextPage = () => {
-    return (
-        <main className="body min-h-screen flex items-center justify-center">
-            <RegisterForm />
-        </main>
-    )
-}
+  verifyToken();
+  return (
+    <main className="body min-h-screen flex items-center justify-center">
+      <RegisterForm />
+    </main>
+  );
+};
 
 export default Register;
